5d58fe4caaad35f1ccf1fe2a0e0d4b69a1564ca4
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-11-19 14:21:02 +0100

    remove blocks and ui update

--- indexScript.js
+++ indexScript.js
@@ -9,6 +9,40 @@ let laenge;
 let hoehe;
 let selected;
 
+function setInputFilter(textbox, inputFilter, errMsg) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function (event) {
+        textbox.addEventListener(event, function (e) {
+            if (inputFilter(this.value)) {
+                // Accepted value
+                if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+                    this.classList.remove("input-error");
+                    this.setCustomValidity("");
+                }
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                // Rejected value - restore the previous one
+                this.classList.add("input-error");
+                this.setCustomValidity(errMsg);
+                this.reportValidity();
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                // Rejected value - nothing to restore
+                this.value = "";
+            }
+        });
+    });
+}
+
+setInputFilter(inputLaenge, function (value) {
+    return /^\d*$/.test(value) && parseInt(value) > 0
+}, "Muss eine Positive Zahl sein")
+setInputFilter(inputHoehe, function (value) {
+    return /^\d*$/.test(value) && parseInt(value) > 0
+}, "Muss eine Positive Zahl sein")
+
 createButton.addEventListener("click", () => {
     laenge = parseInt(inputLaenge.value);
     hoehe = parseInt(inputHoehe.value);
@@ -41,16 +75,16 @@ function select(blockType) {
 function createPlayground() {
     const playground = document.getElementById("playground");
     playground.innerHTML = "";
-    const cellSize = `${80 / (laenge+1)}vw`;
+    const cellSize = `min(${80 / (laenge + 1)}vw, ${50 / hoehe}vh)`;
 
-    for (let i = 0; i < hoehe; i++) {
+    for (let rowIndex = 0; rowIndex < hoehe; rowIndex++) {
         const row = document.createElement("div");
         row.classList.add("row");
 
-        addLamp(row, i, cellSize)
+        addLamp(row, rowIndex, cellSize)
 
-        for (let j = 0; j < laenge; j++) {
-            addCell(row, i, j, cellSize)
+        for (let columnIndex = 0; columnIndex < laenge; columnIndex++) {
+            addCell(row, rowIndex, columnIndex, cellSize)
         }
         playground.appendChild(row);
     }
@@ -59,9 +93,9 @@ function createPlayground() {
 function addCell(row, rowId, cellId, cellSize) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.id = `cell-${rowId}-${cellId}`; // Unique ID based on position
-    cell.style.width = cellSize;
-    cell.style.height = cellSize;
+    cell.id = `cell-${cellId}-${rowId}`; // Unique ID based on position
+    cell.style.width = cellSize
+    cell.style.height = cellSize
     cell.innerHTML = "X"
     cell.style.color = "rgba(115, 115, 115, 0.1)";
     cell.draggable = false
@@ -76,7 +110,8 @@ function addLamp(row, id, cellSize) {
     const lamp = document.createElement("div");
     lamp.classList.add("lamp", "cell", "off");
     lamp.id = `lamp-${id}`;
-    lamp.style.width = lamp.style.height = cellSize
+    lamp.style.width = cellSize
+    lamp.style.height = cellSize
     lamp.addEventListener("click", lampClick)
     row.appendChild(lamp)
 }
@@ -104,18 +139,18 @@ function lampOn(lamp) {
 }
 
 function handleCellHover(event) {
+    const cell = event.target;
+    if (cell.innerHTML !== "X") {
+        const find = findCombined(cell)
+        find.top.style.borderColor = "lightgreen"
+        find.bottom.style.borderColor = "lightgreen"
+        return;
+    }
     if (selected == null) {
         return;
     }
-    const cell = event.target;
-    const [rowIndex, colIndex] = cell.id.split("-").slice(1).map(Number);
-    const cellBelow = document.getElementById(`cell-${rowIndex}-${colIndex + 1}`);
-    if (event.target.id.split("-")[2] === hoehe - 1) {
-        return;
-    }
-    if (cell.innerHTML !== "X" || cellBelow.innerHTML !== "X") {
-        return;
-    }
+    if (!isValid(cell)) return;
+    const cellBelow = findCellBelow(cell)
     cell.style.backgroundColor = "lightgreen";
     cellBelow.style.backgroundColor = "lightgreen";
 }
@@ -123,24 +158,65 @@ function handleCellHover(event) {
 function handleCellMouseOut(event) {
     const cell = event.target;
     cell.style.backgroundColor = "";
-    const [rowIndex, colIndex] = cell.id.split("-").slice(1).map(Number);
-    const cellBelow = document.getElementById(`cell-${rowIndex}-${colIndex + 1}`);
+    const cellBelow = findCellBelow(cell)
     if (cellBelow) {
         cellBelow.style.backgroundColor = "";
     }
+    if (cell.style.borderColor === "lightgreen") {
+        calculateLight()
+    }
+}
+
+function findCellBelow(cell) {
+    const pos = position(cell)
+    return document.getElementById(`cell-${pos.col}-${pos.row + 1}`);
+}
+
+function position(cell) {
+    const [colIndex, rowIndex] = cell.id.split("-").slice(1).map(Number);
+    return {row: rowIndex, col: colIndex}
+}
+
+function isValid(cell) {
+    const cellBelow = findCellBelow(cell)
+    const pos = position(cell)
+    if (pos.row === hoehe - 1) {
+        return false;
+    }
+    return !(cell.innerHTML !== "X" || cellBelow.innerHTML !== "X");
+}
+
+function deleteBlock(cell) {
+    const find = findCombined(cell)
+    find.top.innerHTML = "X"
+    find.bottom.innerHTML = "X"
+    find.top.style.backgroundImage = ""
+    find.bottom.style.backgroundImage = ""
+    calculateLight()
+}
+
+function findCombined(cell) {
+    const pos = position(cell)
+    let row
+    let rowCell
+    for (row = 0; row <= pos.row; row++) {
+        rowCell = document.getElementById(`cell-${pos.col}-${row}`)
+        if (rowCell.innerHTML !== "X") row++;
+    }
+    const below = findCellBelow(rowCell)
+    return {top: rowCell, bottom: below}
 }
 
 function handleCellClick(event) {
     const cell = event.target;
-    const [rowIndex, colIndex] = cell.id.split("-").slice(1).map(Number);
-    const cellBelow = document.getElementById(`cell-${rowIndex}-${colIndex + 1}`);
 
-    if (event.target.id.split("-")[2] == hoehe - 1) {
+    if (cell.innerHTML !== "X") {
+        deleteBlock(cell)
         return;
     }
-    if (cell.innerHTML !== "X" || cellBelow.innerHTML !== "X") {
-        return;
-    }
+
+    if (!isValid(cell)) return;
+    const cellBelow = findCellBelow(cell)
 
     if (selected === blauBB) {
         cell.innerHTML = "B";
@@ -173,7 +249,6 @@ function handleCellClick(event) {
     calculateLight()
 }
 
-
 const clear = document.getElementById("clear");
 
 clear.addEventListener("click", () => {
@@ -183,25 +258,25 @@ clear.addEventListener("click", () => {
 });
 
 function calculateLight() {
-    const lampenWerte = new Array(laenge+1);
+    const lampenWerte = new Array(laenge + 1);
     lampenWerte[0] = new Array(hoehe);
     for (let i = 0; i < hoehe; i++) {
         lampenWerte[0][i] = document.getElementById(`lamp-${i}`).classList.contains("on");
     }
     const bigArray = new Array(laenge);
-    for (let i = 0; i < laenge; i++) {
-        bigArray[i] = new Array(hoehe);
-        for (let j = 0; j < hoehe; j++) {
-            if (document.getElementById(`cell-${i}-${j}`).innerHTML === "B") {
-                bigArray[i][j] = BausteinSegmentEnum.B;
-            } else if (document.getElementById(`cell-${i}-${j}`).innerHTML === "W") {
-                bigArray[i][j] = BausteinSegmentEnum.W;
-            } else if (document.getElementById(`cell-${i}-${j}`).innerHTML === "r") {
-                bigArray[i][j] = BausteinSegmentEnum.r;
-            } else if (document.getElementById(`cell-${i}-${j}`).innerHTML === "R") {
-                bigArray[i][j] = BausteinSegmentEnum.R;
-            } else if (document.getElementById(`cell-${i}-${j}`).innerHTML === "X") {
-                bigArray[i][j] = BausteinSegmentEnum.X;
+    for (let col = 0; col < laenge; col++) {
+        bigArray[col] = new Array(hoehe);
+        for (let row = 0; row < hoehe; row++) {
+            if (document.getElementById(`cell-${col}-${row}`).innerHTML === "B") {
+                bigArray[col][row] = BausteinSegmentEnum.B;
+            } else if (document.getElementById(`cell-${col}-${row}`).innerHTML === "W") {
+                bigArray[col][row] = BausteinSegmentEnum.W;
+            } else if (document.getElementById(`cell-${col}-${row}`).innerHTML === "r") {
+                bigArray[col][row] = BausteinSegmentEnum.r;
+            } else if (document.getElementById(`cell-${col}-${row}`).innerHTML === "R") {
+                bigArray[col][row] = BausteinSegmentEnum.R;
+            } else if (document.getElementById(`cell-${col}-${row}`).innerHTML === "X") {
+                bigArray[col][row] = BausteinSegmentEnum.X;
             }
         }
     }
@@ -211,16 +286,17 @@ function calculateLight() {
     }
     for (let i = 1; i < laenge; i++) {
         for (let j = 0; j < hoehe; j++) {
-            const cell = document.getElementById(`cell-${i-1}-${j}`)
-            if (lampenWerte[i][j] == true) {
+            const cell = document.getElementById(`cell-${i - 1}-${j}`)
+            if (lampenWerte[i][j] === true) {
                 cell.style.border = "3px solid yellow";
-            } if (lampenWerte[i][j] != true) {
+            }
+            if (lampenWerte[i][j] !== true) {
                 cell.style.border = "1px solid black"
             }
             const nextCell = document.getElementById(`cell-${i}-${j}`)
-            if (lampenWerte[i][j] == true && nextCell.innerHTML == "X") {
+            if (lampenWerte[i][j] === true && nextCell.innerHTML === "X") {
                 nextCell.style.backgroundImage = "url('image/licht.jpg')";
-            } else if (lampenWerte[i][j] != true && nextCell.innerHTML == "X") {
+            } else if (lampenWerte[i][j] !== true && nextCell.innerHTML === "X") {
                 nextCell.style.backgroundImage = "";
             }
         }
